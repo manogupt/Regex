32b9d4d18dcbdff9c0e959cc17f7bbd7ea54fca6
manogupt <manojgupta123929@gmail.com>
2022-07-17 17:24:26 +0530

    Added : Validate the Email id

--- src/com/Bridgelabz/RegexExpression.java
+++ src/com/Bridgelabz/RegexExpression.java
@@ -5,26 +5,26 @@ import java.util.regex.Pattern;
 
 public class RegexExpression 
 {
-	static boolean validate(String pattern,String text) {
+	static boolean validate(String pattern, String text) {
 		return Pattern.compile(pattern).matcher(text).matches();
 	}
-	
+
 	public static void main(String[] args) {
-		
-		String pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
-		
+
+        String pattern  = "^[a-zA-Z]+([_+-.][a-zA-Z])*[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2})*$";
+
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please Enter Last Name =>");
-		
+		System.out.println("Please Enter Email id=>");
+
 		String lastName = scanner.next();
-		
+
 		boolean isValid = validate(pattern, lastName);
-		
-		if(isValid) {
-			System.out.println("This is valid last Name.");
-		}else {
-			System.out.println(" Not a valid Name");
+
+		if (isValid) {
+			System.out.println("This is valid Email ID .");
+		} else {
+			System.out.println("Not a valid ID");
 		}
-		
 	}
 }
