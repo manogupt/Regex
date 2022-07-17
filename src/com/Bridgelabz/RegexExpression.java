3f776344e57a34a4cf4ff46b67cf8c51a3d79b6c
manogupt <manojgupta123929@gmail.com>
2022-07-17 17:19:29 +0530

    Added : Validate the last name

--- src/com/Bridgelabz/RegexExpression.java
+++ src/com/Bridgelabz/RegexExpression.java
@@ -1,23 +1,30 @@
 package com.Bridgelabz;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class RegexExpression 
 {
-	static String firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
-
+	static boolean validate(String pattern,String text) {
+		return Pattern.compile(pattern).matcher(text).matches();
+	}
+	
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the first name =>\n");
-		String testName = scan.nextLine();
-
-		if (testName.matches(firstName)) {
-			System.out.println("This is valid first name.");
-		} else {
-			System.out.println("Not a valid name");
+		String pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please Enter Last Name =>");
+		
+		String lastName = scanner.next();
+		
+		boolean isValid = validate(pattern, lastName);
+		
+		if(isValid) {
+			System.out.println("This is valid last Name.");
+		}else {
+			System.out.println(" Not a valid Name");
 		}
+		
 	}
-
-
 }
