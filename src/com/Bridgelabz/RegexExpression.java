b6c10ef25e4e74854487482bdeba6016b3385967
manogupt <manojgupta123929@gmail.com>
2022-07-17 17:32:26 +0530

    Added : Validate the password pattern

--- src/com/Bridgelabz/RegexExpression.java
+++ src/com/Bridgelabz/RegexExpression.java
@@ -5,25 +5,26 @@ import java.util.regex.Pattern;
 
 public class RegexExpression 
 {
-	static boolean validate(String pattern, String text) {
+         static boolean validate(String pattern, String text) {
+		
 		return Pattern.compile(pattern).matcher(text).matches();
 	}
 
 	public static void main(String[] args) {
 
-        String pattern  = "^[-.+]{1}(91)[1-9]{1}[0-9]{9}$";
+		String pattern = "^[a-zA-Z0-9]{8,}$";
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please Enter Mobile No=>");
+		System.out.println("Please Enter value =>");
 
 		String lastName = scanner.next();
 
 		boolean isValid = validate(pattern, lastName);
 
 		if (isValid) {
-			System.out.println("This is valid Number .");
+			System.out.println("This is valid Email ID .");
 		} else {
-			System.out.println("Not a valid Number");
+			System.out.println("Not a valid ID");
 		}
 	}
 }
