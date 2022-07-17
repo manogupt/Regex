acba334b360489f79900435e03986439c65eee79
manogupt <manojgupta123929@gmail.com>
2022-07-17 18:44:39 +0530

    Added : Validate special character and number in password pattern

--- src/com/Bridgelabz/RegexExpression.java
+++ src/com/Bridgelabz/RegexExpression.java
@@ -2,8 +2,9 @@ package com.Bridgelabz;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class RegexExpression {
-
+public class RegexExpression 
+{
+        
 	static boolean validate(String pattern, String text) {
 
 		return Pattern.compile(pattern).matcher(text).matches();
@@ -11,20 +12,19 @@ public class RegexExpression {
 
 	public static void main(String[] args) {
 
-		String pattern = "^[A-Z]{1}[a-zA-Z0-9]{8,}$";
+		String pattern = "^[A-Z]{1}[a-zA-Z0-9]{1,}[!@#$%^&]{1,}[a-zA-Z0-9]{1,}$";
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Please Enter value:=");
+		System.out.println("Please Enter Password =>");
 
 		String lastName = scanner.next();
 
 		boolean isValid = validate(pattern, lastName);
 
 		if (isValid) {
-			System.out.println("This is valid Email ID .");
+			System.out.println("This is valid Password .");
 		} else {
-			System.out.println("Not a valid ID");
+			System.out.println("Not a valid Password");
 		}
 	}
-
 }
